3730e1e333ed08efa6c0beb56a1e26bf12373180
Hatem <hatem.deux@gmail.com>
2020-12-17 17:31:06 -0500

    implements the function (TDD)

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
+const { 
+    diagonalizeMatrix, 
+    diagonalizeMatrixReverse 
+} = require('./diagonalizeMatrix');
+const transpose = require('./transposeMatrix')
+
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    // Error Handling : Validating input
+    if (letters.length === 0 ) throw "Array must not be empty";
+    letters.forEach(element => {
+       if ( !Array.isArray(element)) throw "Array is not 2D"
+    });
+    
+
+    const findWord = function (array, word) {
+        const joinString = array.map(ls => ls.join(''))
+        for (l of joinString) {
+            if (l.includes(word)) return true
+            if (l.includes(word.split('').reverse().join(''))) return true
+        }
+        return false
     }
+
+    return (
+        findWord(letters, word)
+        || findWord(transpose(letters), word)
+        || findWord(diagonalizeMatrix(letters), word)
+        || findWord(diagonalizeMatrixReverse(letters), word)
+    )
 }
 
 module.exports = wordSearch
