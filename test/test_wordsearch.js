aba31f967d48a76d0f5a1c7a17df26e31f326520
Hatem <hatem.deux@gmail.com>
2020-12-17 17:29:18 -0500

    adds test cases for validation, vertical, backwards and diagonal word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,9 +1,23 @@
+const { should, expect } = require('chai');
 const chai = require('chai');
 const assert = chai.assert;
 
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
+  it("should throw an error if array is empty", function() {
+    const fn = () => wordSearch([], 'SEINFELD')
+
+    assert.throws(fn, "Array must not be empty");
+  });
+
+  it("should throw an error if array is not 2-dimentional", function() {
+    const fn = () => wordSearch(
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      'SEINFELD')
+    assert.throws(fn, "Array is not 2D")
+  });
+
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -20,7 +34,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +49,76 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present horizontally (backwards) [#STRETCH]", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LREYSCHW')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LDLGLBISL')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically (backwards) [#STRETCH]", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'QKANYEQFQ')
+
+    assert.isTrue(result);
+  });
+
+  const array = [
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ];
+  describe("## should return true if the word is present diagonally [#STRETCH]", function() {
+    it("South-West to North-East direction and its opposite", () => {
+      assert.isTrue(wordSearch(array,'GREAA')); // NE -> SW
+      assert.isTrue(wordSearch(array,'EDTE')); // SW -> NE
+    });
+    it("South-East to North-West direction and its opposite", () => {
+      assert.isTrue(wordSearch(array,'BBC')); // NW -> SE 
+      assert.isTrue(wordSearch(array,'GAEQ')); // SE -> NW
+    });
+  });
+
+
 });
